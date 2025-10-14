9207b4d0061c24fc19eefdb532d472a8b1553ad5
Maximo <maximoreynosob@gmail.com>
2025-10-14 15:52:49 -0300

    Header

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -4,7 +4,15 @@ import './Header.css'
 function Header(){
     return (
         <>
-            <h1 className='titulo'>Hello World</h1>
+            <div className='header'>
+                <p className='text'>M. Reynoso</p>
+            </div>
+            <div className='options'>
+                <div><a href='#'>Home</a></div>
+                <div><a href='#'>Education</a></div>
+                <div><a href='#'>Skills</a></div>
+                <div><a href='#'>Contact</a></div>
+            </div>
         </>
     )
 }
